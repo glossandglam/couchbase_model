49e5746494f19c79be255967caad307f4acfc164
Jeremy Linder <jlinder@boardpackager.com>
2023-01-27 13:12:09 -0500

    proper options + multi delete

--- lib/couchbase_model/couchbase_wrapper.rb
+++ lib/couchbase_model/couchbase_wrapper.rb
@@ -4,25 +4,49 @@ class CouchbaseModel
       @couchbase = couchbase
     end
 
-    def get(key, options = nil)
-      results = key.is_a?(Array) ? @couchbase.get_multi(key, options) : @couchbase.get(key, options) 
-
-      process_gotten_item results
+    def get(key, options = {})
+      key.is_a?(Array) ? get_multiple(key, options) : get_single(key, options)
     end
 
-    def add(key, item, options = nil)
-      @couchbase.insert key, prepare_for_setting(item), options
+    def add(key, item, options = {}})
+      opts = Couchbase::Options::Insert.new
+      opts.expiry = options[:ttl] if options[:ttl]
+
+      @couchbase.insert key, prepare_for_setting(item), opts
     end
 
-    def set(key, item, options = nil)
+    def set(key, item, options = {})
+      opts = Couchbase::Options::Upsert.new
+      opts.expiry = options[:ttl] if options[:ttl]
+
       @couchbase.upsert key, prepare_for_setting(item), options
     end
 
-    def delete(key, options = nil)
-      @couchbase.remove key, options
+    def delete(key, options = {})
+      key.is_a?(Array) ? remove_multiple(key, options) : remove_single(key, options)
     end
 
     private
+
+    def get_single(key, options = {})
+      opts = Couchbase::Options::GetM.new
+      process_gotten_item @couchbase.get(key, opts)
+    end
+
+    def get_multiple(keys, options = {})
+      opts = Couchbase::Options::GetMulti.new
+      process_gotten_item @couchbase.get_multi(key, opts)
+    end
+
+    def remove_single(key, options = {})
+      opts = Couchbase::Options::Remove.new
+      @couchbase.remove key, opts
+    end
+
+    def remove_multiple(keys, options = {})
+      opts = Couchbase::Options::RemoveMulti.new
+      @couchbase.remove_multi key, opts
+    end
 
     def process_gotten_item(item)
       return item.map {|child| process_gotten_item child } if item.is_a?(Array)
