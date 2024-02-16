c5363c6d4494658768bc87474db92e5ec66f7d0c
Jeremy Linder <jlinder@boardpackager.com>
2024-02-16 00:35:35 -0500

    only allow timeouts

--- lib/couchbase_model/couchbase_wrapper.rb
+++ lib/couchbase_model/couchbase_wrapper.rb
@@ -9,14 +9,14 @@ class CouchbaseModel
     end
 
     def add(key, item, options = {})
-      opts = Couchbase::Options::Insert.new(**options.except(:ttl))
+      opts = Couchbase::Options::Insert.new(**options.slice(:timeout))
       opts.expiry = options[:ttl] if options[:ttl]
 
       @couchbase.insert key, prepare_for_setting(item, key), opts
     end
 
     def set(key, item, options = {})
-      opts = Couchbase::Options::Upsert.new(**options.except(:ttl))
+      opts = Couchbase::Options::Upsert.new(**options.slice(:timeout))
       opts.expiry = options[:ttl] if options[:ttl]
 
       @couchbase.upsert key, prepare_for_setting(item, key), opts
@@ -29,7 +29,7 @@ class CouchbaseModel
     private
 
     def get_single(key, options = {})
-      opts = Couchbase::Options::Get.new(**options)
+      opts = Couchbase::Options::Get.new(**options.slice(:timeout))
       document = begin
         @couchbase.get(key, opts) 
       rescue Couchbase::Error::DocumentNotFound 
@@ -40,18 +40,18 @@ class CouchbaseModel
     end
 
     def get_multiple(keys, options = {})
-      opts = Couchbase::Options::GetMulti.new(**options)
+      opts = Couchbase::Options::GetMulti.new(**options.slice(:timeout))
       process_gotten_item @couchbase.get_multi(keys, opts)
     end
 
     def remove_single(key, options = {})
-      opts = Couchbase::Options::Remove.new(**options)
+      opts = Couchbase::Options::Remove.new(**options.slice(:timeout))
       @couchbase.remove key, opts
     end
 
     def remove_multiple(keys, options = {})
-      opts = Couchbase::Options::RemoveMulti.new(**options)
-      @couchbase.remove_multi key, opts
+      opts = Couchbase::Options::RemoveMulti.new(**options.slice(:timeout))
+      @couchbase.remove_multi keys, opts
     end
 
     def process_gotten_item(item)
@@ -60,16 +60,16 @@ class CouchbaseModel
       item = item&.content
       return item unless item
       return item if item.is_a?(Array)
-      return item unless item.key?("__val")
+      return item unless item.key?('__val')
 
-      item["__val"]
+      item['__val']
     end
 
     def prepare_for_setting(item, key = nil)
       return item if item.is_a?(Hash)
       return item.map {|child| prepare_for_setting(child)} if item.is_a?(Array)
 
-      { "__val" => item, "__key" => key }
+      { '__val' => item, '__key' => key }
     end
   end
 end
